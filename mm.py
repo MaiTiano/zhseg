7ebc68bc50f4ea4224bb0e5b329b70d346600281
scturtle <scturtle@gmail.com>
2012-10-05 12:11:20 +0800

    fraction and log versions of MM

---
+++ mm.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+from __future__ import unicode_literals
+from fractions import Fraction as frac
+import json
+import collections
+
+d = {}
+d.update(json.load(open('dict.json')))
+#d.update(json.load(open('sogou.json')))
+total = long(sum(map(lambda x: d[x], d)))
+
+
+def simplify(f):
+    return frac(1, int(1 / float(f)))
+ 
+def prob(w):
+    if w in d:
+        return d[w]
+    else:
+        return 1 if len(w) == 1 else 0
+ 
+def fenci(s):
+    l = len(s)
+    p = [1 for i in range(l + 1)]
+    t = [None for i in range(l)]
+
+    for i in range(l - 1, -1, -1):
+        p[i], t[i] = max([(frac(prob(s[i:i + k]), total) * p[i + k], k)
+                          for k in range(1, l - i + 1)])
+        p[i] = simplify(p[i])
+
+    print 'sum:', p[0]
+    i = 0
+    while i < l:
+        yield s[i:i + t[i]]
+        i = i + t[i]
+ 
+ 
+if __name__ == '__main__':
+    s = '疾步路过书店隔着玻璃看见埋头阅读的人们很想一砖头砸破他们的平静我就是羡慕嫉妒恨虽然如果我很悠闲但也懒得演绎书店读书一幕'
+    print '|'.join(list(fenci(s))).encode('utf-8')
---
+++ mm2.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+from __future__ import unicode_literals
+from __future__ import division
+import json
+import collections
+import math
+
+d = {}
+d.update(json.load(open('dict.json')))
+#d.update(json.load(open('sogou.json')))
+total = long(sum(map(lambda x: d[x], d)))
+
+log = lambda x: float('-inf') if not x else math.log(x)
+
+def prob(w):
+    if w in d:
+        return d[w]
+    else:
+        return 1 if len(w) == 1 else 0
+ 
+def fenci(s):
+    l = len(s)
+    p = [0 for i in range(l + 1)]
+    t = [None for i in range(l)]
+
+    for i in range(l - 1, -1, -1):
+        p[i], t[i] = max([(log(prob(s[i:i + k]) / total) + p[i + k], k) 
+                          for k in range(1, l - i + 1)])
+
+    print 'sum:', p[0]
+    i = 0
+    while i < l:
+        yield s[i:i + t[i]]
+        i = i + t[i]
+ 
+if __name__ == '__main__':
+    s = '疾步路过书店隔着玻璃看见埋头阅读的人们很想一砖头砸破他们的平静我就是羡慕嫉妒恨虽然如果我很悠闲但也懒得演绎书店读书一幕'
+    print '|'.join(list(fenci(s))).encode('utf-8')
